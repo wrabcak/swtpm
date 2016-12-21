581e4f3eb39bdb46296e942b16052f3fd241f2b8
Stefan Berger <stefanb@linux.vnet.ibm.com>
2016-12-21 17:01:06 -0500

    swtpm: Provide support for TPM2 using --tpm2
    Implement support for TPM2. Some of the capabilities are not supported yet in
    this patch.
    
    Extend the man pages with description for --tpm2.
    
    Missing: configure should probe for needed API calls in libtpms
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- src/swtpm/utils.h
+++ src/swtpm/utils.h
@@ -49,6 +49,6 @@ typedef void (*sighandler_t)(int);
 int install_sighandlers(int pipefd[2], sighandler_t handler);
 int change_process_owner(const char *owner);
 
-void tpmlib_debug_libtpms_parameters(void);
+void tpmlib_debug_libtpms_parameters(TPMLIB_TPMVersion);
 
 #endif /* _SWTPM_UTILS_H_ */
